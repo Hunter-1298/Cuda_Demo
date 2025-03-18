97905547f3b46321967a4595ae3dff6d19c84687
hhayden <hayden1298@hotmail.com>
2025-03-18 17:47:16 -0400

    adding comments

--- simple_matrix.cpp
+++ simple_matrix.cpp
@@ -3,13 +3,14 @@
 #include <cuda_runtime>
 
 // Adds vectors of A and B, size on N into C
+// ThreadIDX is a built in varaible
 __global__ void VecAdd(float *A, float *B, float *C)
 {
     int i = threadIdx.x;
     C[i] = B[i] + A[i];
 }
 
-// Thread IDX is a three component vector
+// Thread IDX is a three component vector,
 // Thread block dimensions Dx,Dy,Dz
 // Thread idx = (x+ yDx +zDxDy)
 __global__ void VecAddThreeComponent(float A[N][N], float B[N][N], float C[N][N])
@@ -21,6 +22,7 @@ __global__ void VecAddThreeComponent(float A[N][N], float B[N][N], float C[N][N]
 int main()
 {
     // VecAdd function
+    // Sets number of threads in <<<...>>>
     // VecAdd<<<1, N>>>(A, B, C);
 
     // VecAdd 3 dimension
