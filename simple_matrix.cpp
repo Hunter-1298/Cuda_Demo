d2bf0b4f8047bf9f33860aca86b29175f06ef0d9
hhayden <hayden1298@hotmail.com>
2025-03-18 17:16:36 -0400

    VecAdd

---
+++ simple_matrix.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <memory>
+#include <cuda_runtime>
+
+// Adds vectors of A and B, size on N into C
+__global__ void VecAdd(float *A, float *B, float *C)
+{
+    int i = threadIdx.x;
+    C[i] = B[i] + A[i];
+}
+
+// Thread IDX is a three component vector
+// Thread block dimensions Dx,Dy,Dz
+// Thread idx = (x+ yDx +zDxDy)
+__global__ void VecAddThreeComponent(float A[N][N], float B[N][N], float C[N][N])
+{
+    int i = threadIdx.x int j = threadIdx.y;
+    C[i][j] = A[i][j] + B[i][j];
+}
+
+int main()
+{
+    // VecAdd function
+    // VecAdd<<<1, N>>>(A, B, C);
+
+    // VecAdd 3 dimension
+    int numBlocks = 1;
+    dim3 threadsPerBlock(N, N);
+    VecAddThreeComponent<<<numBlocks, threadsPerBlock>>>(A, B, C);
+
+    return 0;
+}
